5fbff55fdb3fe9e329b7c8a2d8cdfc149e7062f2
yozu <yasuo@ozu.email>
2024-09-21 16:44:59 +0900

    WIP

--- macro/lib.rs
+++ macro/lib.rs
@@ -1242,7 +1242,6 @@ pub fn _sumtrait_internal(input: TokenStream1) -> TokenStream1 {
     .into()
 }
 
-#[doc(hidden)]
 #[proc_macro_error]
 #[proc_macro_attribute]
 pub fn sumtrait(attr: TokenStream1, input: TokenStream1) -> TokenStream1 {
